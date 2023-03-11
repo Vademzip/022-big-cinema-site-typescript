256319d492698fdcf04d3f2ae3ec27120d88da5c
Vademzip <vadim.sardarov@yandex.ru>
2023-03-11 19:49:01 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, {useState} from 'react'
+import './App.css'
+import Header from "./components/Header";
+import {Container} from "@mui/material";
+
+function App() {
+
+    return (
+            <Header/>
+    )
+}
+
+export default App
