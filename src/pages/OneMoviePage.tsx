94350b6585da5189cce2dc0c7fbe14e6ba93731f
Vademzip <vadim.sardarov@yandex.ru>
2023-03-17 04:51:35 +0300

    0.1.5.1
    - Исправлены ошибки типизации
    - Немного косметических улучшений

--- src/pages/OneMoviePage.tsx
+++ src/pages/OneMoviePage.tsx
@@ -1,6 +1,6 @@
 import React, {FC, useEffect, useState} from 'react';
 import {useParams} from "react-router-dom";
-import { styled as muiStyled } from '@mui/material/styles';
+import {styled as muiStyled} from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Unstable_Grid2';
@@ -15,8 +15,8 @@ const MovieInfo = styled.div`
   color: white;
   align-items: center;
   row-gap: 15px;
-  
-  
+
+
 `
 const MovieTitle = styled.div`
   font-size: 36px;
@@ -25,6 +25,10 @@ const MovieTitle = styled.div`
 `
 const MovieCreationYear = styled.div`
   color: #cbcbcb;
+  &:hover{
+    color: white;
+    cursor: pointer;
+  }
 `
 
 
@@ -32,30 +36,49 @@ const MovieTags = styled.div`
   display: flex;
   column-gap: 5px;
   color: #cbcbcb;
+  &> div:hover{
+    color: white;
+    cursor: pointer;
+  }
 `
 
 const MovieProducer = styled.div`
-  
+
 `
 
 const MovieActors = styled.div`
-  
+
 `
-
-
 
 
 const Poster = styled.img`
-    max-width: 100%;
-    border-radius: 10px;
+  max-width: 100%;
+  border-radius: 10px;
 `
 
 
-const OneMoviePage:FC = () => {
+const OneMoviePage: FC = () => {
     const {id} = useParams();
-    const [movie, setMovie] = useState<IMovieInfo>(null)
+    const [movie, setMovie] = useState<IMovieInfo| undefined>({
+        "name": "Аватар: Путь воды",
+        "genre": ["фантастика", "фэнтези", "боевик", "приключения"],
+        "producer": "Джеймс Кэмерон",
+        "posterUrl": "https://avatars.mds.yandex.net/get-kinopoisk-image/6201401/2e51cb8b-fd51-4166-84a2-63559733baac/1920x",
+        "creationYear": 2022,
+        "rating": 7.8,
+        "cast": [
+            "Сэм Уортингтон",
+            "Зои Салдана",
+            "Сигурни Уивер",
+            "Стивен Лэнг",
+            "Кейт Уинслет"
+        ],
+        "id": 6464616946156464
+    })
     useEffect(() => {
-        setMovie(data.find((movie) => movie.id === +id));
+        if (id) {
+            setMovie(data.find((movie) => movie.id === +id));
+        }
     }, [id]);
     return (
         <Box sx={{flexGrow: 1}}>
